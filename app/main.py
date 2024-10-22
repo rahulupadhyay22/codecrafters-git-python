4245a995dd1edc12ae4374dbe46773636b48bdec
rahulupadhyay22 <rahulu825@gmail.com>
2024-10-22 20:12:33 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,86 +1,84 @@
+from operator import itemgetter
 import sys
-import os
 import zlib
 import hashlib
-def read_binary_object(sha):
-    with open(f".git/objects/{sha[:2]}/{sha[2:]}", "rb") as f:
-        data = zlib.decompress(f.read())
-        header, content = data.split(b"\0", 1)
-        return content
-def create_blob_entry(path, write=True):
-    with open(path, "rb") as f:
-        data = f.read()
-        header = f"blob {len(data)}\0".encode("utf-8")
-        store = header + data
-        sha = hashlib.sha1(store).hexdigest()
-        if write:
-            os.makedirs(f".git/objects/{sha[:2]}", exist_ok=True)
-            with open(f".git/objects/{sha[:2]}/{sha[2:]}", "wb") as f:
-                f.write(zlib.compress(store))
-    return sha
-def write_tree(path: str):
-    if os.path.isfile(path):
-        return create_blob_entry(path)
-    contents = sorted(
-        os.listdir(path),
-        key=lambda x: x if os.path.isfile(os.path.join(path, x)) else f"{x}/",
-    )
-    s = b""
-    for item in contents:
-        if item == ".git":
-            continue
-        full = os.path.join(path, item)
-        if os.path.isfile(full):
-            s += f"100644 {item}\0".encode()
-        else:
-            s += f"40000 {item}\0".encode()
-        sha1 = int.to_bytes(int(write_tree(full), base=16), length=20, byteorder="big")
-        s += sha1
-    s = f"tree {len(s)}\0".encode() + s
-    sha1 = hashlib.sha1(s).hexdigest()
-    os.makedirs(f".git/objects/{sha1[:2]}", exist_ok=True)
-    with open(f".git/objects/{sha1[:2]}/{sha1[2:]}", "wb") as f:
-        f.write(zlib.compress(s))
-    return sha1
+from pathlib import Path
+from typing import Tuple, List
+def read_object(parent: Path, sha: str) -> bytes:
+    pre = sha[:2]
+    post = sha[2:]
+    p = parent / ".git" / "objects" / pre / post
+    bs = p.read_bytes()
+    _, content = zlib.decompress(bs).split(b"\0", maxsplit=1)
+    return content
+def write_object(parent: Path, ty: str, content: bytes) -> str:
+    content = ty.encode() + b" " + f"{len(content)}".encode() + b"\0" + content
+    hash = hashlib.sha1(content, usedforsecurity=False).hexdigest()
+    compressed_content = zlib.compress(content)
+    pre = hash[:2]
+    post = hash[2:]
+    p = parent / ".git" / "objects" / pre / post
+    p.parent.mkdir(parents=True, exist_ok=True)
+    p.write_bytes(compressed_content)
+    return hash
 def main():
-    command = sys.argv[1]
-    if command == "init":
-        os.mkdir(".git")
-        os.mkdir(".git/objects")
-        os.mkdir(".git/refs")
-        with open(".git/HEAD", "w") as f:
-            f.write("ref: refs/heads/main\n")
-        print("Initialized git directory")
-    elif command == "cat-file":
-        # We don't care about the options yet, but we need to extract them anyway
-        if sys.argv[2] == "-p":
-            hash = sys.argv[3]
-            content = read_binary_object(hash)
-            print(content.decode("utf-8"), end="")
-    elif command == "hash-object":
-        if sys.argv[2] == "-w":
-            file = sys.argv[3]
-            with open(file, "rb") as f:
-                data = f.read()
-                header = f"blob {len(data)}\0".encode("utf-8")
-                store = header + data
-                sha = hashlib.sha1(store).hexdigest()
-                os.makedirs(f".git/objects/{sha[:2]}", exist_ok=True)
-                with open(f".git/objects/{sha[:2]}/{sha[2:]}", "wb") as f:
-                    f.write(zlib.compress(store))
-                print(sha)
-    elif command == "ls-tree":
-        param, hash = sys.argv[2], sys.argv[3]
-        if param == "--name-only":
-            binary_data = read_binary_object(hash)
-            while binary_data:
-                mode, binary_data = binary_data.split(b"\0", 1)
-                _, name = mode.split()
-                binary_data = binary_data[20:]
-                print(name.decode("utf-8"))
-    elif command == "write-tree":
-        print(write_tree("./"))
-    else:
-        raise RuntimeError(f"Unknown command #{command}")
+    match sys.argv[1:]:
+        case ["init"]:
+            Path(".git/").mkdir(parents=True)
+            Path(".git/objects").mkdir(parents=True)
+            Path(".git/refs").mkdir(parents=True)
+            Path(".git/HEAD").write_text("ref: refs/heads/main\n")
+            print("Initialized git directory")
+        case ["cat-file", "-p", blob_sha]:
+            sys.stdout.buffer.write(read_object(Path("."), blob_sha))
+        case ["hash-object", "-w", path]:
+            hash = write_object(Path("."), "blob", Path(path).read_bytes())
+            print(hash)
+        case ["ls-tree", "--name-only", tree_sha]:
+            items = []
+            contents = read_object(Path("."), tree_sha)
+            while contents:
+                mode, contents = contents.split(b" ", 1)
+                name, contents = contents.split(b"\0", 1)
+                sha = contents[:20]
+                contents = contents[20:]
+                items.append((mode.decode(), name.decode(), sha.hex()))
+            for _, name, _ in items:
+                print(name)
+        case ["write-tree"]:
+            parent = Path(".")
+            def toEntry(p: Path, exclude_git: bool = False) -> Tuple[str, str, str]:
+                mode = "40000" if p.is_dir() else "100644"
+                if p.is_dir():
+                    entries: List[Tuple[str, str, str]] = []
+                    for child in p.iterdir():
+                        if exclude_git and child.name == ".git":
+                            continue
+                        entries.append(toEntry(child))
+                    s_entries = sorted(entries, key=itemgetter(1))
+                    b_entries = b"".join(
+                        m.encode() + b" " + n.encode() + b"\0" + bytes.fromhex(h)
+                        for (m, n, h) in s_entries
+                    )
+                    hash = write_object(parent, "tree", b_entries)
+                    return (mode, p.name, hash)
+                else:
+                    hash = write_object(parent, "blob", p.read_bytes())
+                    return (mode, p.name, hash)
+            (_, _, hash) = toEntry(Path(".").absolute(), True)
+            print(hash)
+        case ["commit-tree", tree_sha, "-p", commit_sha, "-m", message]:
+            contents = b"".join(
+                [
+                    b"tree %b\n" % tree_sha.encode(),
+                    b"parent %b\n" % commit_sha.encode(),
+                    b"author ggzor <30713864+ggzor@users.noreply.github.com> 1714599041 -0600\n",
+                    b"committer ggzor <30713864+ggzor@users.noreply.github.com> 1714599041 -0600\n\n",
+                    message.encode(),
+                    b"\n",
+                ]
+            )
+            hash = write_object(Path("."), "commit", contents)
+            print(hash)
 if __name__ == "__main__":
     main()
