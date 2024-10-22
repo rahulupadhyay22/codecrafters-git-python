d7feb8ddeb5a675029759847f4525a934de96503
rahulupadhyay22 <rahulu825@gmail.com>
2024-10-22 20:06:41 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,24 +1,24 @@
 import sys
 import os
-
-
+import zlib
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    # print("Logs from your program will appear here!")
-
-    # Uncomment this block to pass the first stage
-    
-     command = sys.argv[1]
-     if command == "init":
-         os.mkdir(".git")
-         os.mkdir(".git/objects")
-         os.mkdir(".git/refs")
-         with open(".git/HEAD", "w") as f:
-             f.write("ref: refs/heads/main\n")
-         print("Initialized git directory")
-     else:
-         raise RuntimeError(f"Unknown command #{command}")
-
+    command = sys.argv[1]
+    if command == "init":
+        os.mkdir(".git")
+        os.mkdir(".git/objects")
+        os.mkdir(".git/refs")
+        with open(".git/HEAD", "w") as f:
+            f.write("ref: refs/heads/main\n")
+        print("Initialized git directory")
+    if command == "cat-file":
+        if sys.argv[2] == "-p":
+            blob_sha = sys.argv[3]
+            with open(f".git/objects/{blob_sha[:2]}/{blob_sha[2:]}", "rb") as f:
+                raw = zlib.decompress(f.read())
+                header, content = raw.split(b"\0", maxsplit=1)
+                print(content.decode("utf-8"), end="")
+    else:
+        raise RuntimeError(f"Unknown command #{command}")
 
 if __name__ == "__main__":
     main()
